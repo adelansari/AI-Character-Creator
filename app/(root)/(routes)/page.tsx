14fcd40cd17a0916998cd3894d9e3ec358b16747
Adel Ansari <adelansari.a@gmailcom>
2023-09-11 18:04:51 +0300

    character display grids and logic complete

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,15 +1,43 @@
 import { Categories } from "@/components/categories";
+import { Characters } from "@/components/characters";
 import { SearchInput } from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 
-const RootPage = async () => {
-    const categories = await prismadb.category.findMany();
-    return (
-        <div className="h-full p-4 space-y-2">
-            <SearchInput />
-            <Categories data={categories} />
-        </div>
-    );
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
 }
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
+  const data = await prismadb.character.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
+  const categories = await prismadb.category.findMany();
+  return (
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
+      <Categories data={categories} />
+      <Characters data={data} />
+    </div>
+  );
+};
 
 export default RootPage;
