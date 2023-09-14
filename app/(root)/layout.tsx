935a03e8b53418fffa18803931f5a847ce6a9477
Adel Ansari <adelansari.a@gmailcom>
2023-09-15 00:13:30 +0300

    metadata edit + favicon

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
 import { checkSubscription } from "@/lib/subscription";
@@ -5,12 +6,13 @@ import { checkSubscription } from "@/lib/subscription";
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const isPremium = await checkSubscription();
   return (
-    <div className="h-full">
+    <div className="flex flex-col min-h-screen">
       <Navbar isPremium={isPremium} />
       <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
         <Sidebar isPremium={isPremium} />
       </div>
-      <main className="md:pl-20 pt-16 h-full">{children}</main>
+      <main className="md:pl-20 pt-16 flex-grow">{children}</main>
+      <Footer />
     </div>
   );
 };
