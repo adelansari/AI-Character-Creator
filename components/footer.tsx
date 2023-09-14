fce8915af39785582e19cd6b4db4bb713b6b09b4
Adel Ansari <adelansari.a@gmailcom>
2023-09-15 00:13:38 +0300

    footer

---
+++ components/footer.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export const Footer = () => {
+  return (
+    <footer className="py-3 px-2 mt-auto bg-gray-200 dark:bg-stone-900">
+      <div className="max-w-sm mx-auto">
+        <p className="text-center text-sm">
+          Made by Adel -{" "}
+          <a
+            href="https://github.com/adelansari/AI-Character-Creator"
+            className="text-blue-600 font-bold"
+          >
+            GitHub
+          </a>{" "}
+        </p>
+      </div>
+    </footer>
+  );
+};
